6d46973174103dfce49893b1a0cd2043c48d8b0b
Ivan <chivvasregal94@gmail.com>
2023-02-14 15:18:25 +0300

    info05

---
+++ info05/Program.cs
@@ -0,0 +1,23 @@
+﻿double a;
+double b;
+double c;
+a = 44;
+b = 5;
+c = 78;
+
+if (a > b) 
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+if (b > c)  
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
